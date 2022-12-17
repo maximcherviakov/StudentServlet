2ee2f8401673272bd36f2f43efa45f9d2797031f
maximcherviakov <cherviakovmaxim@gmail.com>
2022-12-17 13:17:43 +0200

    DB connection lab was implemented

--- src/main/java/com/example/studentservletllab/StudentAdd.java
+++ src/main/java/com/example/studentservletllab/StudentAdd.java
@@ -4,38 +4,73 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
-import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
 import java.util.LinkedList;
 import java.util.List;
 
-@WebServlet(name = "StudentAdd", value = "/StudentAdd")
+@WebServlet(name = "Default", urlPatterns = "/", loadOnStartup = 1)
 public class StudentAdd extends HttpServlet {
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
         processRequest(req, resp);
     }
 
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        HttpSession session = request.getSession();
-        List<Student> students = (List<Student>) session.getAttribute("students");
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
+        processRequest(req, resp);
+    }
 
-        if (students == null) {
-            students = new LinkedList<>();
-            session.setAttribute("students", students);
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) {
+        PrintWriter writer = null;
+        try {
+            writer = response.getWriter();
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (Exception exception) {
+            exception.printStackTrace(writer);
+            writer.print(exception.getMessage());
         }
 
-        if (request.getParameter("name") != "" || request.getParameter("surname") != "") {
-            Student student = new Student(request.getParameter("name"),
-                    request.getParameter("surname"),
-                    request.getParameter("age"),
-                    request.getParameter("email"),
-                    request.getParameter("group"),
-                    request.getParameter("faculty"));
-            students.add(student);
-        }
+        try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3311/university", "root", "root")) {
+            if (request.getParameter("name") != null && request.getParameter("surname") != null) {
+                PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO student (name_, surname, age, email, group_, faculty) " +
+                        "VALUES (?, ?, ?, ?, ?, ?)");
 
-        response.sendRedirect("index.jsp");
+                preparedStatement.setString(1, request.getParameter("name"));
+                preparedStatement.setString(2, request.getParameter("surname"));
+                preparedStatement.setString(3, request.getParameter("age"));
+                preparedStatement.setString(4, request.getParameter("email"));
+                preparedStatement.setString(5, request.getParameter("group"));
+                preparedStatement.setString(6, request.getParameter("faculty"));
+
+                preparedStatement.executeUpdate();
+                response.sendRedirect("./");
+            }
+
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM student");
+            List<Student> students = new LinkedList<>();
+
+            while(resultSet.next()) {
+                Student student = new Student(
+                        resultSet.getString(2),
+                        resultSet.getString(3),
+                        resultSet.getString(4),
+                        resultSet.getString(5),
+                        resultSet.getString(6),
+                        resultSet.getString(7)
+                );
+                students.add(student);
+            }
+
+            request.setAttribute("students", students);
+            request.getRequestDispatcher("view.jsp").forward(request, response);
+        } catch (SQLException exception) {
+            writer.print(exception.getMessage());
+            exception.printStackTrace();
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
     }
 }
